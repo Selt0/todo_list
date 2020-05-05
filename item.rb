58fea4ca419aec5d2a12988f3e16344f2d5799a5
Selt0 <chewedgum12@gmail.com>
2020-05-05 18:32:35 -0500

    Initialize item class. Can set and rewrite title, description, and deadline

--- item.rb
+++ item.rb
@@ -1,5 +1,20 @@
 class Item
-  def initialize
-    
+
+  def self.valid_date?(date)
+    begin
+      Date.iso8601(date)
+      true
+    rescue => exception
+      false
+    end
   end
+
+  attr_accessor :title, :deadline, :description
+
+  def initialize(title, deadline, description)
+    @title = title
+    @deadline = deadline
+    @description = description
+  end
+
 end
